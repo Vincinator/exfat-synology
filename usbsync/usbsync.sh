659ce43213c28d08045e2fd3ac96d1e65f3ab4dd
Vincent Riesop <Vincent.Riesop@gmail.com>
2020-04-19 22:13:50 +0200

    added photo&video import

--- usbsync/usbsync.sh
+++ usbsync/usbsync.sh
@@ -4,22 +4,11 @@
 #/bin/usbsync.sh [mountdevice] [mountpoint]
 #/bin/usbsync.sh /dev/sdu1 /volume1/usbexfat/usbshare1
 
-#require settingss
-DefaultDir=/volume1/PhotoImported
-Settings=$DefaultDir/usbsync.cfg
-if [ ! -f "$Settings" ]; then
-	exit 0
-fi	
-MountDevice="$1"
-MountPoint="$2"
-PID="$$"
-t=$(date +%s)
-SysLog=/tmp/usbsync.running
-echo "[PID$PID] $t $MountDevice $MountPoint" >> $SysLog
+
 function CleanUpLog() {
 	sed -i '/\ '"$t"'\ /d' $SysLog
 }
-sleep 2
+
 function CheckRunning() {
 	n=1
 	c=0
@@ -40,26 +29,6 @@ function CheckRunning() {
 		((n+=1))
 	done
 }
-CheckRunning
-ls -r "$MountPoint"
-DevName=$(/bin/mount.bin | grep "$(dirname $MountPoint)/$(basename $MountPoint)" | awk '{ print $1 }')
-DiskName=$(echo "$DevName" | sed 's/[0-9]//g')
-#load settings
-SourceDir=$(cat "$Settings" | grep 'SourceDir')
-if [ -n "$SourceDir" ]; then
-	SrcDir=$MountPoint/${SourceDir#*:}
-else
-	SrcDir=$MountPoint/
-fi
-ImportDir=$(cat "$Settings" | grep 'ImportDir')
-if [ -n "$ImportDir" ]; then
-	DestDir=${ImportDir#*:}
-else
-	DestDir=$DefaultDir
-fi
-if [ ! -d "$DestDir" ]; then
-	mkdir -p "$DestDir"
-fi
 function CheckDisk() {
 	echo "Disk"${DiskInfo#*"$DiskName"} > /tmp/usb.hash"$PID"
 	DevInfo=$(fdisk -l | grep "$DevName")
@@ -70,34 +39,7 @@ function CheckDisk() {
 	echo "USBHash: $UUID" >> $DestDir/usbhash.log
 	rm -f /tmp/usb.hash"$PID"
 }
-CheckDisk
-AutoSync=$(cat "$Settings" | grep 'AutoSync')
-AutoSync=${AutoSync#*:}
-if [ "$AutoSync" != "true" ]; then
-	CleanUpLog
-	exit 0
-fi
-USBHash=$(cat "$Settings" | grep 'USBHash' | grep "$UUID")
-if [ -z "$USBHash" ]; then
-	CleanUpLog
-	exit 0
-fi	
-RsyncLog=$DestDir/import_latest.log
-rm -f $RsyncLog
-FileExtension=$(cat "$Settings" | grep 'FileExtension')
-if [ -n "$FileExtension" ]; then
-	FileExtension=${FileExtension#*:}
-	FileExtension=($(echo "$FileExtension" | sed 's/*/\\*/g' | sed 's/;/ /g'))
-	for s in ${FileExtension[@]}
-	do
-		if [ "$s" == "\*" ]||[ "$s" == "\*.\*" ]; then
-			FileExtension=""
-			break
-		fi
-	done
-else
-	FileExtension=""
-fi
+
 function UmountDisk() {
 	#auto unmount
 	AutoUnmount=$(cat "$Settings" | grep 'AutoUnmount')
@@ -125,69 +67,176 @@ function UmountDisk() {
 		"$EXIFNAME" &
 	fi
 }
-#load history record
-OldFiles=$DestDir/\@eaDir/copied.log
-if [ ! -f "$OldFiles" ]; then
-	touch "$OldFiles"
-fi
-AllFiles=/tmp/all.files"$PID".list
-NewFiles=/tmp/new.files"$PID".list
-SyncList=/tmp/sync.files"$PID".list
-if [ ! -d "$SrcDir" ]; then
+
+function DoRepl() {
+
+	CheckDisk
+	AutoSync=$(cat "$Settings" | grep 'AutoSync')
+	AutoSync=${AutoSync#*:}
+	if [ "$AutoSync" != "true" ]; then
+		CleanUpLog
+		return 0
+	fi
+	USBHash=$(cat "$Settings" | grep 'USBHash' | grep "$UUID")
+	#if [ -z "$USBHash" ]; then
+	#	CleanUpLog
+	#	 exit 0
+	#fi	
+
+	RsyncLog=$DestDir/import_latest.log
+	rm -f $RsyncLog
+	FileExtension=$(cat "$Settings" | grep 'FileExtension')
+	if [ -n "$FileExtension" ]; then
+		FileExtension=${FileExtension#*:}
+		FileExtension=($(echo "$FileExtension" | sed 's/*/\\*/g' | sed 's/;/ /g'))
+		for s in ${FileExtension[@]}
+		do
+			if [ "$s" == "\*" ]||[ "$s" == "\*.\*" ]; then
+				FileExtension=""
+				break
+			fi
+		done
+	else
+		FileExtension=""
+	fi
+
+	#load history record
+	OldFiles=$DestDir/\@eaDir/copied.log
+	if [ ! -f "$OldFiles" ]; then
+		touch "$OldFiles"
+	fi
+	AllFiles=/tmp/all.files"$PID".list
+	NewFiles=/tmp/new.files"$PID".list
+	SyncList=/tmp/sync.files"$PID".list
+	if [ ! -d "$SrcDir" ]; then
+		echo `date` > $RsyncLog
+		echo "USBHash: $UUID" >> $RsyncLog
+		echo "Found 0 New File(s) in" $SrcDir >> $RsyncLog
+		UmountDisk
+		sleep 5
+		CleanUpLog
+		return 0
+	fi
+	#save file list to AllFiles
+	if [ -z "$FileExtension" ]; then
+		#echo "find all"
+		find $SrcDir -type f -name "*.*" | sed 's/ /\\ /g' | xargs ls -go --full-time | cut -c 14- > $AllFiles
+	else
+		rm -f $AllFiles
+		for s in ${FileExtension[@]}
+		do
+			#echo "find $s"
+			find $SrcDir -type f -name "*.*" | grep -i "\.$s$" | sed 's/ /\\ /g' | xargs ls -go --full-time | cut -c 14- >> $AllFiles
+		done
+	fi
+	#cat $AllFiles
+	#exit 0
+	n=1
+	c=0
+	while ((n<=$(cat $AllFiles | wc -l)))
+	do
+		FileInfo=$(cat $AllFiles | sed -n "${n} p")
+		if [ -n "$FileInfo" ]; then
+			MainInfo=${FileInfo%%/*}`basename ${FileInfo#*/}`
+			IsOldFile=`cat $OldFiles | grep "$(echo $MainInfo | sed 's/ /\\ /g')"`
+			if [ -z "$IsOldFile" ]; then
+				echo $MainInfo >> $NewFiles
+				echo ${FileInfo#*$SrcDir/} >> $SyncList
+				((c+=1))
+			fi
+		fi
+		((n+=1))
+	done
+	#
 	echo `date` > $RsyncLog
 	echo "USBHash: $UUID" >> $RsyncLog
-	echo "Found 0 New File(s) in" $SrcDir >> $RsyncLog
-	UmountDisk
-	sleep 5
-	CleanUpLog
-	exit 0
-fi
-#save file list to AllFiles
-if [ -z "$FileExtension" ]; then
-	#echo "find all"
-	find $SrcDir -type f -name "*.*" | sed 's/ /\\ /g' | xargs ls -go --full-time | cut -c 14- > $AllFiles
-else
-	rm -f $AllFiles
-	for s in ${FileExtension[@]}
-	do
-		#echo "find $s"
-		find $SrcDir -type f -name "*.*" | grep -i "\.$s$" | sed 's/ /\\ /g' | xargs ls -go --full-time | cut -c 14- >> $AllFiles
-	done
-fi
-#cat $AllFiles
-#exit 0
-n=1
-c=0
-while ((n<=$(cat $AllFiles | wc -l)))
-do
-	FileInfo=$(cat $AllFiles | sed -n "${n} p")
-	if [ -n "$FileInfo" ]; then
-		MainInfo=${FileInfo%%/*}`basename ${FileInfo#*/}`
-		IsOldFile=`cat $OldFiles | grep "$(echo $MainInfo | sed 's/ /\\ /g')"`
-		if [ -z "$IsOldFile" ]; then
-			echo $MainInfo >> $NewFiles
-			echo ${FileInfo#*$SrcDir/} >> $SyncList
-			((c+=1))
-		fi
+	echo "Found" $c "New File(s) in" $SrcDir >> $RsyncLog
+	#sync new files
+	if [ -f "$SyncList" ]; then
+		rsync -av --files-from=$SyncList $SrcDir $DestDir >> $RsyncLog
+		rm -f $SyncList
 	fi
-	((n+=1))
-done
-#
-echo `date` > $RsyncLog
-echo "USBHash: $UUID" >> $RsyncLog
-echo "Found" $c "New File(s) in" $SrcDir >> $RsyncLog
-#sync new files
-if [ -f "$SyncList" ]; then
-	rsync -av --files-from=$SyncList $SrcDir $DestDir >> $RsyncLog
-	rm -f $SyncList
+	#save list to OldFiles
+	if [ -f "$NewFiles" ]; then
+		cat $NewFiles >> $OldFiles
+		rm -f $NewFiles
+	fi
+} 
+
+
+#require settingss
+DefaultDir=/volume1/PhotoImported
+Settings=$DefaultDir/usbsync.cfg
+if [ ! -f "$Settings" ]; then
+	exit 0
+fi	
+MountDevice="$1"
+MountPoint="$2"
+PID="$$"
+t=$(date +%s)
+SysLog=/tmp/usbsync.running
+echo "[PID$PID] $t $MountDevice $MountPoint" >> $SysLog
+
+sleep 2
+
+
+CheckRunning
+ls -r "$MountPoint"
+DevName=$(/bin/mount.bin | grep "$(dirname $MountPoint)/$(basename $MountPoint)" | awk '{ print $1 }')
+DiskName=$(echo "$DevName" | sed 's/[0-9]//g')
+#load settings
+
+
+# 1) Photo Import
+
+
+SourceDirPhoto=$(cat "$Settings" | grep 'SourceDirPhoto')
+if [ -n "$SourceDirPhoto" ]; then
+	SrcDir=$MountPoint/${SourceDirPhoto#*:}
+else
+	SrcDir=$MountPoint/
 fi
-#save list to OldFiles
-if [ -f "$NewFiles" ]; then
-	cat $NewFiles >> $OldFiles
-	rm -f $NewFiles
+ImportDirPhoto=$(cat "$Settings" | grep 'ImportDirPhoto')
+if [ -n "$ImportDirPhoto" ]; then
+	DestDir=${ImportDirPhoto#*:}
+else
+	DestDir=$DefaultDir
 fi
+if [ ! -d "$DestDir" ]; then
+	mkdir -p "$DestDir"
+fi
+
+
+DoRepl
+
 rm -f $AllFiles
 
+# 2) Video Import
+SourceDirVideo=$(cat "$Settings" | grep 'SourceDirVideo')
+if [ -n "$SourceDirVideo" ]; then
+	SrcDir=$MountPoint/${SourceDirVideo#*:}
+else
+	echo "Import Photo Dir is not set" >> $RsyncLog
+	return 0
+fi
+
+ImportDirVideo=$(cat "$Settings" | grep 'ImportDirVideo')
+if [ -n "$ImportDirVideo" ]; then
+	DestDir=${ImportDirVideo#*:}
+else 
+	echo "Import Video Dir is not set" >> $RsyncLog
+	return 0
+fi
+if [ ! -d "$DestDir" ]; then
+	mkdir -p "$DestDir"
+fi
+
+
+DoRepl
+
+# 3) Cleanup ...
+
+rm -f $AllFiles
 UmountDisk
 sleep 10
 CleanUpLog
